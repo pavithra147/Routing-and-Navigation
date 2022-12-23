4a23d7095eca2a97120bd2560fc9018e4cfac445
pavithra147 <pavithra.jayavel@aspiresys.com>
2022-12-23 12:50:27 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [              //each route in this array is javascript object that contains two properties  
-    {path:'Home',component:HomeComponent,
-    children:  [
-      {
-        path:'sign-in',component:SignInComponent,},
+    {path:'Home',title:'Home',component:HomeComponent,
+    // children:  [
+    //   {
+    //     path:'sign-in ',component:SignInComponent,},
       
-    ],
+    // ],
   },
-    {path:'About',component: AboutComponent}, 
+    { path:'sign-in',title:'Home',component:SignInComponent},
+    {path:'About',title:'About',component: AboutComponent}, 
     // {path:'',redirectTo:'/Home',pathMatch:'full'},//"pathMatch tell the router how to match the URL"  //redirect to 'Home' "path:'' means to use the initial relative url"                 //Here we define each route
     {path:'**',component: PageNotFoundComponent},//wildcard route for 404 page //last route with ** is wildcard route.Router selects this route if the requested url doesn't match any of the paths earlier in list
 
