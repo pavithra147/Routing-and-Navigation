6416f81028f0b9556f47db5908aa3f8f0aacc853
pavithra147 <pavithra.jayavel@aspiresys.com>
2023-02-02 21:31:31 +0530

    Modifications

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [              //each route in this array is javascript o
     { path:'sign-in',title:'Home',component:SignInComponent},
     {path:'About',title:'About',component: AboutComponent}, 
     // {path:'',redirectTo:'/Home',pathMatch:'full'},//"pathMatch tell the router how to match the URL"  //redirect to 'Home' "path:'' means to use the initial relative url"                 //Here we define each route
-    {path:'**',component: PageNotFoundComponent},//wildcard route for 404 page //last route with ** is wildcard route.Router selects this route if the requested url doesn't match any of the paths earlier in list
+    //{path:'**',component: PageNotFoundComponent},//wildcard route for 404 page //last route with ** is wildcard route.Router selects this route if the requested url doesn't match any of the paths earlier in list
 
 ];
 
